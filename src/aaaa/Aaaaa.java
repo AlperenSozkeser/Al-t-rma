03ccf24edbf1c93ca33719c8fe1afa1ef93b028e
Alperen <alperencemalsozkeser@gmail.com>
2021-04-14 01:53:53 +0200

    dememeye devam

--- src/aaaa/Aaaaa.java
+++ src/aaaa/Aaaaa.java
@@ -4,7 +4,7 @@ public class Aaaaa {
 
 	public static void main(String[] args) {
 		System.out.println("alýþtýrmalara devam");
-
+		System.out.println("aslýnda oradaki isimle alakasý yok istediðini koyabilrisnn");
 	}
 
 }
