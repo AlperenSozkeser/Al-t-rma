d7c5041fcdaad45b0c09b53e406aec0418678d22
AlperenSozkeser <81852161+AlperenSozkeser@users.noreply.github.com>
2021-04-14 02:40:17 +0200

    Update Aaaaa.java

--- src/aaaa/Aaaaa.java
+++ src/aaaa/Aaaaa.java
@@ -5,7 +5,7 @@ public class Aaaaa {
 	public static void main(String[] args) {
 		System.out.println("alıştırmalara devam");
 		System.out.println("aslında oradaki isimle alakası yok istediğini koyabilrisnn");
-		Systemçout.println("Uzak sunucudan değiiklikleri aldık");
+		System.out.println("Uzak sunucudan degisiklikleri aldik");
 		
 	}
 
